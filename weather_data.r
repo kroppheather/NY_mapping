76ff9d40981ad09dc301b453bba2bc5dde11d746
kroppheather <kroppheather@gmail.com>
2020-06-17 15:00:46 -0400

    setting up weather data

---
+++ weather_data.r
@@ -0,0 +1,7 @@
+######################################################
+###### Script for organizing NY weather data    ######
+###### from NOAA                                ######
+######################################################
+
+datW <- read.csv("c:\\Users\\hkropp\\Google Drive\\NY_spring\\2185792.csv", 
+		na.strings=c("NA",""))
