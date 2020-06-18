e07f6776c682c58e83ce7b12b9fc84e509deee3a
kroppheather <kroppheather@gmail.com>
2020-06-18 10:37:45 -0400

    used rnoaa package but temperatures not downloading for stations

--- weather_data.r
+++ weather_data.r
@@ -2,10 +2,23 @@
 ###### Script for organizing NY weather data    ######
 ###### from NOAA                                ######
 ######################################################
-install.packages("rnoaa")
+library(dplyr)
 library(rnoaa)
+#set directory for data
 
 Sys.setenv(RNOAA_GHCND_BASE_URL =
 "ftp://ftp.ncdc.noaa.gov/pub/data/ghcn/daily/all")
-test <- ghcnd("USC00300023")
+#stations to work with
+stations <- read.csv("C:\\Users\\hkropp\\Google Drive\\NY_spring\\NY_stations.csv")
 
+datW <- ghcnd(stationid = paste(stations$station_id[3]))
+datV <- ghcnd_splitvars(datW)
+
+datP <- datV[[1]]
+
+datTmax <- datV[[]]
+
+datW2 <- ghcnd(stationid = paste(stations$station_id[501]))
+datV2 <- ghcnd_splitvars(datW2)
+
+test <- ghcnd_search(paste(stations$station_id[220]), var=c("TMAX", "TMIN","TAVG","PRCP"))
